120e7758dbe2ee87042fada03b3327d0f4934886
qim <m17854257603@163.com>
2019-01-07 15:59:57 +0800

    + statistics api

--- node-blog/models/tool.js
+++ node-blog/models/tool.js
@@ -37,8 +37,63 @@ const queryByCondition = (req, res, next) => {
 	}
 }
 
+const getYearPvStatistics = (req, res, next) => {
+	let startTime = moment().year(moment().year()).startOf('year').format('YYYY-MM-DD HH:mm')
+	let endTime = moment().year(moment().year()).endOf('year').format('YYYY-MM-DD HH:mm')
+	Model.Post
+		.where('time')
+		.gte(startTime).lte(endTime)
+		.select('pv comments time')
+		.exec((err, docs) => {
+			if(err) return next(err)
+			let result = []
+			for(let i = 1; i < 13; i++) {
+				let m = `${moment().year()}-${i>9?i: '0'+i}`
+				result.push({
+					month: m,
+					pno: 0,
+					pv: 0,
+					com: 0
+				})
+			}
+			docs.map(e => {
+				let d = moment(e.time).format('MM')
+				result[d-1].pv += e.pv
+				result[d-1].pno++
+				result[d-1].com += e.comments.length					
+			})
+			res.json(result)
+		})
+}
+
+const getTagsWordCloud = (req, res, next) => {
+	Model.Post.find({}, (err, docs) => {
+		if(err) return next(err)
+		let result = []
+		docs.map(e => {
+			e.tags.map(tag => {
+				let flag = result.some(item => {
+					if(tag == item.name) {
+						return item.value++
+					}
+					return false
+				})
+				if(!flag){
+					result.push({
+						name: tag,
+						value: 1
+					})
+				}
+			})
+		})
+		res.json(result)
+	})
+}
+
 
 
 router.post('/tool/query', queryByCondition)
+router.get('/tool/getYearPvStatistics', getYearPvStatistics)
+router.get('/tool/getTagsWordCloud', getTagsWordCloud)
 
 module.exports = router
